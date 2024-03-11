5a84cf28515dbb47ee4112ac836b6e01f6d5995b
mohdazlan <azlan@pmu.edu.my>
2024-03-11 09:31:39 +0800

    first commit

---
+++ currencyConverter/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.nuMYR = New System.Windows.Forms.NumericUpDown()
+        Me.cboCurrency = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        CType(Me.nuMYR, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'nuMYR
+        '
+        Me.nuMYR.Location = New System.Drawing.Point(3, 12)
+        Me.nuMYR.Name = "nuMYR"
+        Me.nuMYR.Size = New System.Drawing.Size(120, 20)
+        Me.nuMYR.TabIndex = 0
+        '
+        'cboCurrency
+        '
+        Me.cboCurrency.FormattingEnabled = True
+        Me.cboCurrency.Items.AddRange(New Object() {"BRL", "BND", "INR"})
+        Me.cboCurrency.Location = New System.Drawing.Point(206, 12)
+        Me.cboCurrency.Name = "cboCurrency"
+        Me.cboCurrency.Size = New System.Drawing.Size(121, 21)
+        Me.cboCurrency.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(129, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 18)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "MYR -->"
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Font = New System.Drawing.Font("Mongolian Baiti", 11.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.Location = New System.Drawing.Point(3, 39)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(0, 16)
+        Me.lblResult.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(339, 72)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cboCurrency)
+        Me.Controls.Add(Me.nuMYR)
+        Me.Name = "Form1"
+        Me.Text = "Currency Converter"
+        CType(Me.nuMYR, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents nuMYR As NumericUpDown
+    Friend WithEvents cboCurrency As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblResult As Label
+End Class
---
+++ currencyConverter/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub nuMYR_ValueChanged(sender As Object, e As EventArgs) Handles nuMYR.ValueChanged
+        If cboCurrency.Text = "BRL" Then
+            lblResult.Text = Val(nuMYR.Value) * 1.07
+            lblResult.BackColor = Color.LightYellow
+        ElseIf cboCurrency.Text = "BND" Then
+            lblResult.Text = Val(nuMYR.Value) * 0.28
+            lblResult.BackColor = Color.Orange
+        ElseIf cboCurrency.Text = "INR" Then
+            lblResult.Text = Val(nuMYR.Value) * 17.67
+            lblResult.BackColor = Color.Wheat
+        Else
+            MessageBox.Show("Please Select A Currency First", "Problem", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+End Class
